0a570c3c8f129ee16e921bef1c6bae7c3893aacc
Pedrocardo005 <pedro.cardoso@ssig.tech>
2023-10-12 13:00:59 -0300

    Comeca a por as informacoes do pokemon no modal

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -2,8 +2,37 @@ var modal = document.getElementById("myModal");
 
 var span = document.getElementsByClassName("close")[0];
 
-function openModal() {
+/**
+ * @param {Number} id 
+ */
+function openModal(id) {
+    const url = 'https://pokeapi.co/api/v2/pokemon/' + id;
+    const topModal = document.getElementById('top-modal');
+    const bottomModal = document.getElementById('bottom-modal');
+
     modal.style.display = "block";
+
+    fetch(url)
+        .then((response) => response.json())
+        .then((pokemon) => {
+            const pokeFomarted = convertPokeApiDetailToPokemon(pokemon)
+            const topHtml = `
+                <span class="number">#${pokeFomarted.number}</span>
+                <span class="name">${pokeFomarted.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                        ${pokeFomarted.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                    </ol>
+                </div>
+                <div class="img-pokemon">
+                    <img src="${pokeFomarted.photo}" alt="${pokeFomarted.name}" width="150px">
+                </div>
+            `;
+
+            topModal.classList.add(pokeFomarted.type);
+            topModal.innerHTML = topHtml;
+
+        });
 }
 
 span.onclick = function () {
